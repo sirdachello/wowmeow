07b2eab7fc812f8abc6ee24eced7295c7ff6d432
Valentine Kliuchev <valentynkliuchev@gmail.com>
2025-04-17 21:25:27 +0300

    /games game card missing a MoreButton sometimes on safari

--- src/app/components/MoreButton.tsx
+++ src/app/components/MoreButton.tsx
@@ -1,8 +1,15 @@
 export default function MoreButton() {
   return (
-    <div className="before:bg-customBlack relative before:absolute before:inset-[-8px] before:block before:transition-all before:duration-200 before:[clip-path:polygon(3%_8%,100%_0,97%_92%,0_100%)] md:before:inset-[-12px] md:before:w-[304px]">
+    <div className="relative">
+      {/* Safari-friendly pseudo-element styling */}
+      <div className="absolute top-[-8px] right-[-8px] bottom-[-8px] left-[-8px] block bg-customBlack w-[124px] md:w-[304px] md:top-[-12px] md:right-[-12px] md:bottom-[-12px] md:left-[-12px]" style={{
+        clipPath: "polygon(3% 8%, 100% 0, 97% 92%, 0 100%)",
+        WebkitClipPath: "polygon(3% 8%, 100% 0, 97% 92%, 0 100%)",
+        transition: "all 0.2s",
+      }}></div>
+      
       <button
-        className={`bg-customYellow focus:bg-customYellow-highlight hover:bg-customYellow-highlight relative z-10 h-[40px] w-[100px] cursor-pointer transition-colors duration-200 md:h-[66px] md:w-[280px]`}
+        className="bg-customYellow hover:bg-customYellow-highlight focus:bg-customYellow-highlight relative z-10 h-[40px] w-[108px] cursor-pointer transition-colors duration-200 md:h-[66px] md:w-[280px]"
       >
         <span className="font-bold uppercase md:text-(length:--font-md)">
           More
