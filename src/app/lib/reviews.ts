c4135a5249c481d28446502386c7cee5411bff02
Valentine Kliuchev <valentynkliuchev@gmail.com>
2025-04-24 13:00:03 +0300

    new review added

--- src/app/lib/reviews.ts
+++ src/app/lib/reviews.ts
@@ -5,6 +5,12 @@ export type Review = {
 }
 export const reviews: Review[] = [
     {
+      name: "Alexey Harlamov",
+      position: "Founder Legend17 games",
+      reviewText:
+      "Shoutout to the squad that hooked up my match3 game with those fire icons! These guys ain’t playin around - every piece pops like candy on steroids, straight up addicting. Colors? Vibin! Crispy clean! They even nailed the tiny details. Communication was smooth too. No ghostin, just pure hustle and dope results. 10/10, zero cap."
+    },
+    {
       name: "John Doe",
       position: "Product Director (under NDA agreement)",
       reviewText:
